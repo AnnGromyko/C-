c21d7dc97eca178eeb443c4172f46933b64c8d71
AnnGromyko <94791778+AnnGromyko@users.noreply.github.com>
2022-04-16 17:01:28 +0300

    Add files via upload

---
+++ Lab2_/Lab2_/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_
+{
+    class Circle : Figure
+    {
+        public double Radius { get; set; }
+
+        public override double GetArea()
+        {
+            return 3.14 * Radius * Radius;
+        }
+
+    }
+}
---
+++ Lab2_/Lab2_/Figure.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_
+{
+    public abstract class Figure
+    {
+        public string Name { get; set; }
+
+        public abstract double GetArea();
+    }
+}
+
