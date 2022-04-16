ffb5cb60240163b73eb6942d460a7a48786c0da2
AnnGromyko <94791778+AnnGromyko@users.noreply.github.com>
2022-04-16 17:00:07 +0300

    Add files via upload

---
+++ Lab1/Lab1/Complex.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Complex
+    {
+        public double Real { get; set; }
+        public double Imag { get; set; }
+
+        //Сложение
+        public void Add(Complex x)
+        {
+            Real += x.Real;
+            Imag += x.Imag;
+        }
+
+        //Вычитание
+        public void Substract(Complex x)
+        {
+            Real -= x.Real;
+            Imag -= x.Imag;
+        }
+
+        //Умнножение
+        public void Multiply(Complex x)
+        {
+            Real *= x.Real;
+            Imag *= x.Imag;
+        }
+
+        //Деление
+        public void Divide(Complex x)
+        {
+            Real /= x.Real;
+            Imag /= x.Imag;
+        }
+    }
+}
